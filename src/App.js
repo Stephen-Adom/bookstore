2938820f770e9e108910271f0aaa6c01b8261075
Stephen Adom Addae <saaddae003@st.ug.edu.gh>
2023-03-31 07:45:18 +0000

    used router provider

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { Navbar } from './features';
-import { Home, Categories } from './pages';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <section className="w-[80%] mx-auto mt-5">
-        <Routes>
-          <Route exact path="/" element={<Home />}> </Route>
-          <Route path="/categories" element={<Categories />} />
-        </Routes>
+        <Outlet />
       </section>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { RouterProvider } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import router from './routes/routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
 
