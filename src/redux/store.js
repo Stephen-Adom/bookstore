f21146e997031bc4208302e583d3d1dd8df0f1d6
Stephen Adom Addae <saaddae003@st.ug.edu.gh>
2023-04-03 09:14:13 +0000

    create book slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import bookSlice from './bookSlice/bookSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    books: bookSlice,
+  },
 });
 
 export default store;
